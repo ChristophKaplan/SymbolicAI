3776cf67c795a97715341a4710e320a5136aa8af
Christoph Kaplan <christoph.kaplan@wooga.net>
2026-06-11 12:49:40 +0200

    FirstOrderLogic: public API

---
+++ FirstOrderLogic/FirstOrderLogic/FirstOrderLogic/FirstOrderLogic.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LRParser.CFG;
+using LRParser.Language;
+using LRParser.Lexer;
+
+namespace FirstOrderLogic {
+    public enum Terminal
+    {
+        Open,
+        Comma,
+        Close,
+        Identifier,
+        Conjunction,
+        Disjunction,
+        Implication,
+        Negation,
+        Boolean,
+        Quantifier,
+        Biconditional,
+        TimeAttribute
+    }
+
+    public enum NonTerminal
+    {
+        LangObject,
+        AtomicSentence,
+        Term,
+        TermList,
+        Sentence,
+        ComplexSentence,
+        LogicalOperator,
+        ComplexSentenceUnary
+    }
+
+    public class FirstOrderLogic : Language<Terminal, NonTerminal>
+    {
+        protected override TokenDefinition<Terminal>[] SetUpTokenDefinitions()
+        {
+            return new[]
+            {
+                new TokenDefinition<Terminal>(Terminal.Open, "\\("),
+                new TokenDefinition<Terminal>(Terminal.Comma, ","),
+                new TokenDefinition<Terminal>(Terminal.Close, "\\)"),
+                new TokenDefinition<Terminal>(Terminal.Conjunction, "AND|&&|\u2227"),
+                new TokenDefinition<Terminal>(Terminal.Disjunction, "OR|\\|\\||\u2228"),
+                new TokenDefinition<Terminal>(Terminal.Implication, "IMPLIES|=>|\u21d2"),
+                new TokenDefinition<Terminal>(Terminal.Biconditional, "IFF|<=>|\u21d4"),
+                new TokenDefinition<Terminal>(Terminal.Negation, "NOT|!|-|~|\u00ac"),
+                new TokenDefinition<Terminal>(Terminal.Boolean, "TRUE|FALSE|\u22a4|\u22a5"),
+                new TokenDefinition<Terminal>(Terminal.Quantifier, "FORALL|EXISTS|\u2200|\u2203"),
+                new TokenDefinition<Terminal>(Terminal.TimeAttribute, "\\^[0-9]"),
+                new TokenDefinition<Terminal>(Terminal.Identifier, "[a-zA-Z0-9]+"),
+            };
+        }
+
+        protected override void SetUpGrammar()
+        {
+            AddRule(rhs => rhs[0].Attribute, NonTerminal.LangObject, NonTerminal.Sentence);
+
+            AddRule(rhs => rhs[1].Attribute, NonTerminal.Sentence, Terminal.Open, NonTerminal.Sentence, Terminal.Close);
+            AddRule(rhs => rhs[0].Attribute, NonTerminal.Sentence, NonTerminal.ComplexSentence);
+            AddRule(rhs => rhs[0].Attribute, NonTerminal.Sentence, NonTerminal.AtomicSentence);
+            AddRule(rhs =>
+            {
+                var boolean = ((LexValue)rhs[0].Attribute).ToLogicalConstant();
+                return new Proposition(Connective.SymbolToString(boolean));
+            }, NonTerminal.Sentence, Terminal.Boolean);
+
+            AddRule(rhs =>
+            {
+                var quantifierSymbol = ((LexValue)rhs[0].Attribute).ToLogicalConstant();
+                var variableString = ((LexValue)rhs[1].Attribute).Value;
+                var sentence = (Sentence)rhs[2].Attribute;
+                return new ComplexSentence(new Quantifier(quantifierSymbol, new Variable(variableString)), sentence);
+            }, NonTerminal.ComplexSentence, Terminal.Quantifier, Terminal.Identifier, NonTerminal.Sentence);
+
+            AddRule(rhs =>
+            {
+                var atomic = (Sentence)rhs[0].Attribute;
+                var extArray = (ArrayValue)rhs[1].Attribute;
+                var connective = (Connective)extArray.Value[0];
+                var sentence = (Sentence)extArray.Value[1];
+                return new ComplexSentence(atomic, connective, sentence);
+            }, NonTerminal.ComplexSentence, NonTerminal.AtomicSentence, NonTerminal.ComplexSentenceUnary);
+
+            AddRule(rhs =>
+            {
+                var atomic = (Sentence)rhs[1].Attribute;
+                var extArray = (ArrayValue)rhs[3].Attribute;
+                var connective = (Connective)extArray.Value[0];
+                var sentence = (Sentence)extArray.Value[1];
+                return new ComplexSentence(atomic, connective, sentence);
+            }, NonTerminal.ComplexSentence, Terminal.Open, NonTerminal.Sentence, Terminal.Close, NonTerminal.ComplexSentenceUnary);
+
+            AddRule(rhs =>
+            {
+                var extArray = (ArrayValue)rhs[0].Attribute;
+                var negation = (Connective)extArray.Value[0];
+                var sentence = (Sentence)extArray.Value[1];
+                return new ComplexSentence(negation, sentence);
+            }, NonTerminal.ComplexSentence, NonTerminal.ComplexSentenceUnary);
+
+            AddRule(rhs =>
+            {
+                var connective = (Connective)rhs[0].Attribute;
+                var sentences = (Sentence)rhs[1].Attribute;
+                return new ArrayValue(connective, sentences);
+            }, NonTerminal.ComplexSentenceUnary, NonTerminal.LogicalOperator, NonTerminal.Sentence);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                return new Proposition(symbol);
+            }, NonTerminal.AtomicSentence, Terminal.Identifier);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                var timeValue = int.Parse(((LexValue)rhs[1].Attribute).Value[1..]);
+                return new Proposition(symbol, timeValue);
+            }, NonTerminal.AtomicSentence, Terminal.Identifier, Terminal.TimeAttribute);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                var terms = ((ArrayValue)rhs[2].Attribute).Value.Select(lo => (Term)lo).ToArray();
+                return new Predicate(symbol, terms);
+            }, NonTerminal.AtomicSentence, Terminal.Identifier, Terminal.Open, NonTerminal.TermList, Terminal.Close);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                var terms = ((ArrayValue)rhs[2].Attribute).Value.Select(lo => (Term)lo).ToArray();
+                var timeValue = int.Parse(((LexValue)rhs[4].Attribute).Value[1..]);
+                return new Predicate(symbol, terms, timeValue);
+            }, NonTerminal.AtomicSentence, Terminal.Identifier, Terminal.Open, NonTerminal.TermList, Terminal.Close, Terminal.TimeAttribute);
+
+            AddRule(rhs => new ArrayValue(rhs[0].Attribute), NonTerminal.TermList, NonTerminal.Term);
+            AddRule(rhs =>
+            {
+                var list = (ArrayValue)rhs[0].Attribute;
+                list.Add(rhs[2].Attribute);
+                return list;
+            }, NonTerminal.TermList, NonTerminal.TermList, Terminal.Comma, NonTerminal.Term);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                var variableList = new[] { "x", "y", "z", "w" };
+                return variableList.Contains(symbol)
+                    ? (ILanguageObject)new Variable(symbol)
+                    : new Constant(symbol);
+            }, NonTerminal.Term, Terminal.Identifier);
+
+            AddRule(rhs =>
+            {
+                var symbol = ((LexValue)rhs[0].Attribute).Value;
+                var terms = ((ArrayValue)rhs[2].Attribute).Value.Select(lo => (Term)lo).ToArray();
+                return new Function(symbol, terms);
+            }, NonTerminal.Term, Terminal.Identifier, Terminal.Open, NonTerminal.TermList, Terminal.Close);
+
+            AddRule(GetConnective, NonTerminal.LogicalOperator, Terminal.Conjunction);
+            AddRule(GetConnective, NonTerminal.LogicalOperator, Terminal.Disjunction);
+            AddRule(GetConnective, NonTerminal.LogicalOperator, Terminal.Implication);
+            AddRule(GetConnective, NonTerminal.LogicalOperator, Terminal.Biconditional);
+            AddRule(GetConnective, NonTerminal.LogicalOperator, Terminal.Negation);
+        }
+
+        ILanguageObject GetConnective(Symbol[] rhs) => new Connective(((LexValue)rhs[0].Attribute).ToLogicalConstant());
+    
+        public override ILanguageObject TryParse(string input)
+        {
+            var langObj = base.TryParse(input);
+            return langObj;
+        }
+    
+        public List<ILanguageObject> TryParse(List<string> inputList)
+        {
+            var langObjList = new List<ILanguageObject>();
+            foreach (var input in inputList) {
+                langObjList.Add(TryParse(input));
+            }
+            return langObjList;
+        }
+    }
+}
---
+++ FirstOrderLogic/FirstOrderLogic/FirstOrderLogic/FirstOrderLogicExtensions.cs
@@ -0,0 +1,227 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using LogHelper;
+using LRParser.Language;
+
+namespace FirstOrderLogic {
+    public static class FirstOrderLogicExtensions
+    {
+        public static Connective.LogicSymbol ToLogicalConstant(this LexValue lexValue) {
+            switch (lexValue.Value) {
+                case "OR":
+                case "||":
+                case "\u2228":
+                    return Connective.LogicSymbol.DISJUNCTION;
+                case "AND":
+                case "&&":
+                case "\u2227":
+                    return Connective.LogicSymbol.CONJUNCTION;
+                case "NOT":
+                case "!":
+                case "-":
+                case "~":
+                case "\u00ac":
+                    return Connective.LogicSymbol.NEGATION;
+                case "IFF":
+                case "<=>":
+                case "\u21d4":
+                    return Connective.LogicSymbol.BICONDITIONAL;
+                case "IMPLIES":
+                case "=>":
+                case "\u21d2":
+                    return Connective.LogicSymbol.IMPLICATION;
+                case "TRUE":
+                case "\u22a4":
+                    return Connective.LogicSymbol.TRUE;
+                case "FALSE":
+                case "\u22a5":
+                    return Connective.LogicSymbol.FALSE;
+                case "FORALL":
+                case "\u2200":
+                    return Connective.LogicSymbol.UNIVERSAL;
+                case "EXISTS":
+                case "\u2203":
+                    return Connective.LogicSymbol.EXISTENTIAL;
+                
+                default:
+                    throw new Exception($"Unknown Logic Symbol: {lexValue}");
+            }
+        }
+    
+        public static ISentence ConnectSentences(this FirstOrderLogic logic, List<ISentence> sentences, Connective.LogicSymbol connective = Connective.LogicSymbol.CONJUNCTION) {
+            switch (sentences.Count) {
+                case 0:
+                    throw new Exception("No sentences to connect.");
+                case 1:
+                    return sentences[0];
+            }
+
+            var conjunct = new ComplexSentence(sentences[0], connective, sentences[1]);
+            for (var i = 2; i < sentences.Count; i++) {
+                conjunct = new ComplexSentence(conjunct,connective, sentences[i]);
+            }
+
+            return conjunct;
+        }
+    
+        private delegate void TransformationDelegate(ref ISentence sentence);
+
+        public static ISentence ToPrenexForm(this FirstOrderLogic logic, ISentence sentence, out List<ISentence> steps) {
+            steps = new List<ISentence>();
+            return ToPrenexFormCore(sentence, steps);
+        }
+
+        // Trace-free overload: skips cloning each intermediate step into a `steps` list. The provers
+        // (Resolution, Theory.Entails) don't need the trace, and that per-step deep clone dominates
+        // normalisation cost, so this is markedly cheaper on the hot path.
+        public static ISentence ToPrenexForm(this FirstOrderLogic logic, ISentence sentence) =>
+            ToPrenexFormCore(sentence, null);
+
+        private static ISentence ToPrenexFormCore(ISentence sentence, List<ISentence>? steps) {
+            var clone = sentence.Clone();
+
+            var transformations = new List<TransformationDelegate> {
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.SimplifyConstants, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.DissolveBiconditional, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.DissolveImplication, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.PushNegation, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.DoubleNegation, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.Absorption, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.AssociationAndIdem, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.PullQuantifier, ref s),
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.RemoveDuplicateQuantifier, ref s),
+            };
+
+            while (true) {
+                var start = clone.Clone();
+                foreach (var transform in transformations) {
+                    transform(ref clone);
+                    steps?.Add(clone.Clone());
+                }
+                if (start.Equals(clone)) {
+                    break;
+                }
+            }
+
+            return clone;
+        }
+
+        public static ISentence ToConjunctiveNormalForm(this FirstOrderLogic logic, ISentence sentence, out List<ISentence> steps) {
+            steps = new List<ISentence>();
+            return ToConjunctiveNormalFormCore(sentence, steps);
+        }
+
+        // Trace-free overload — see ToPrenexForm(logic, sentence).
+        public static ISentence ToConjunctiveNormalForm(this FirstOrderLogic logic, ISentence sentence) =>
+            ToConjunctiveNormalFormCore(sentence, null);
+
+        private static ISentence ToConjunctiveNormalFormCore(ISentence sentence, List<ISentence>? steps) {
+            var clone = ToPrenexFormCore(sentence, steps).Clone();
+
+            var transformations = new List<TransformationDelegate> {
+                (ref ISentence s) => TransformationFOL.Transform(TransformationFOL.EquivType.DistributionOfDisjunction, ref s)
+            };
+
+            while (true) {
+                var start = clone.Clone();
+                foreach (var transform in transformations) {
+                    transform(ref clone);
+                    steps?.Add(clone.Clone());
+                }
+                if (start.Equals(clone)) {
+                    break;
+                }
+            }
+
+            if(!clone.IsCNF()) { throw new Exception("Sentence is not in CNF"); }
+            return clone;
+        }
+
+        public static ISentence SkolemForm(this FirstOrderLogic logic, ISentence sentence) {
+            var clone = sentence.Clone();
+
+            // Expects PNF, where the quantifiers form a linear prefix. Walk the prefix in order,
+            // tracking the universals currently in scope. Each existential is replaced by a Skolem
+            // term over exactly the universals that enclose it, using a fresh unique symbol:
+            //   - no enclosing universals  → Skolem constant   (e.g. sk1)
+            //   - enclosing universals u…  → Skolem function    (e.g. sk1(u, …))
+            var substitution = new Dictionary<Variable, Function>();
+            var universalsInScope = new List<Variable>();
+            var skolemCounter = 0;
+
+            var current = clone;
+            while (current is IComplexSentence { IsQuantifier: true } quantified) {
+                var quantifier = (Quantifier)quantified.Connective;
+                if (quantifier.Symbol == Connective.LogicSymbol.UNIVERSAL) {
+                    universalsInScope.Add(quantifier.Variable);
+                }
+                else {
+                    var args = universalsInScope
+                        .Select(v => (Term)new Variable(v.TermSymbol))
+                        .ToArray();
+                    substitution.Add(quantifier.Variable, new Function($"sk{++skolemCounter}", args));
+                }
+
+                current = quantified.Children[0];
+            }
+
+            foreach (var variable in substitution.Keys) {
+                clone.SubstituteTerm(variable, substitution[variable]);
+            }
+
+            //remove quantifiers
+            TransformationFOL.Transform(TransformationFOL.EquivType.RemoveQuantifier, ref clone);
+
+            return clone;
+        }
+    
+        public static List<Clause> GetClauseSet(this ISentence sentence, List<Clause>? clauseSet = null) {
+            if (!sentence.IsCNF()) { throw new Exception("Sentence is not in CNF"); }
+        
+            clauseSet ??= new List<Clause>();
+
+            if(sentence.IsDisjunctionOfLiterals())
+            {
+                // Only the clause leaves need a defensive copy; interior conjunction nodes don't.
+                var clauseList = sentence.Clone().GetLiterals();
+                clauseSet.Add(new Clause(clauseList.ToArray()));
+                return clauseSet;
+            }
+        
+            foreach (var child in sentence.Children)
+            {
+                child.GetClauseSet(clauseSet);
+            }
+        
+            return clauseSet;
+        }
+
+        public static bool TryGetBinary(
+            this ISentence sentence, string symbol, string firstTerm, out string? secondTerm) {
+            secondTerm = null;
+            if (sentence is not IPredicate wrapper) return false;
+
+            var pred = wrapper.GetPredicate();
+            if (pred.Symbol != symbol) return false;
+
+            var terms = pred.Terms;
+            if (terms == null || terms.Length < 2) return false;
+            if (terms[0].ToString() != firstTerm) return false;
+
+            secondTerm = terms[1].ToString();
+            return !string.IsNullOrEmpty(secondTerm);
+        }
+
+        public static List<ISentence> GetInstancesOverTime(this ISentence sentence, int from, int to) {
+            var sentences = new List<ISentence>();
+            for (var i = from; i < to; i++) {
+                var clone = sentence.Clone();
+                clone.AddTime(i);
+                sentences.Add(clone);
+            }
+
+            return sentences;
+        }
+    }
+}
